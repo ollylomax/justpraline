ff204a49586349928d087b40da21bd3202e9b33f
ollylomax <ollylomax@yahoo.com>
2023-08-16 12:04:52 +0000

    Prepare for heroku deployment

--- just_praline/settings.py
+++ just_praline/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['just-praline.herokuapp.com', 'localhost', '*']
+ALLOWED_HOSTS = ['justpraline.herokuapp.com', 'localhost', '*']
 
 
 # Application definition
